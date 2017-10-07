37270be0c76a8dfc0f223710302a884d9cba2e42
b0rko <32580027+b0rko@users.noreply.github.com>
2017-10-07 16:57:46 +0200

    Zadanie2

--- js/app.js
+++ js/app.js
@@ -20,8 +20,70 @@ var dropdown = document.querySelector('.dropdown')
       dropdown.style.display= 'none';
   });   
     
-//Zadanie 2    
+//Zadanie 2
+var buttons = document.querySelectorAll('.read-more');
+    console.log(buttons); 
+    
+console.log(buttons[0].previousElementSibling);
+
+
+    function showHide() {
+        
+      var textArea = this.previousElementSibling;
+    
+      if (textArea.style.display === 'none' || textArea.style.display === '') {
+        textArea.style.display = 'block';
+        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      } else {
+        textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+      }
+    buttons[0].addEventListener('click', showHide);
+    }
+
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', showHide);
+  }
+});
+   
     
     
     
-     });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
